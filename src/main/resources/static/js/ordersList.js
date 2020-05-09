c86f4c87923cca208a0d46b27b34a77040d8a70d
huang jiajia <huangjiajia@hzqisheng.com>
2020-05-09 17:31:57 +0800

    订单支付退订功能限制

--- src/main/resources/static/js/ordersList.js
+++ src/main/resources/static/js/ordersList.js
@@ -1,34 +1,45 @@
 function unsubscribe(e) {
     var oId=e.name;
-    $.ajax({
-        dataType:'json',
-        type: "post",
-        url: "/orders/unsubscribe",    //向后端请求数据的url
-        data:{oId:oId},
-        success: function (data) {
-            window.location.reload();
+    var oStatus=$("#otatus").val;
+    if (oStatus!="支付完成"){
+        alert("只有支付完成状态可退订")
+    }
+    else {
+        $.ajax({
+            dataType: 'json',
+            type: "post",
+            url: "/orders/unsubscribe",    //向后端请求数据的url
+            data: {oId: oId},
+            success: function (data) {
+                window.location.reload();
 
-        },error:function(xhr,state,errorThrown){
-            alert("退订失败");
-        }
-    });
+            }, error: function (xhr, state, errorThrown) {
+                alert("退订失败");
+            }
+        });
+    }
 
 };
 function payOrders(e) {
     var oId=e.name;
-    $.ajax({
-        dataType:'json',
-        type: "post",
-        url: "/orders/pay",    //向后端请求数据的url
-        data:{oId:oId},
-        success: function (data) {
-            window.location.reload();
-        },
-    error:function(xhr,state,errorThrown){
-        alert("支付失败");
+    var oStatus=$("#otatus").val;
+    if (oStatus=="退订"||oStatus=="超时"||oStatus=="支付完成"){
+        alert("只有预订状态可支付")
     }
-    });
-
+    else{
+        $.ajax({
+            dataType:'json',
+            type: "post",
+            url: "/orders/pay",    //向后端请求数据的url
+            data:{oId:oId},
+            success: function (data) {
+                window.location.reload();
+            },
+        error:function(xhr,state,errorThrown){
+            alert("支付失败");
+        }
+        });
+    }
 }
 function deleteDetail(e) {
     var odId=e.name;
